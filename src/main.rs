3ec387ec04a7e2eaeacc760ec1a92e0817a54899
Kalabuk Dzmitry <dima@proxima.one>
2022-05-26 11:45:44 +0300

    Parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use clap::Parser;
 use grpc::GrpcService;
 use proto::state_manager_service_server::StateManagerServiceServer;
-use service::in_memory::InMemoryStateManager;
+use service::persistent::PersistentStateManager;
+use storage::rocksdb::RocksdbStorage;
 use tonic::transport::Server;
 
 mod grpc;
@@ -11,10 +13,22 @@ mod proto {
   tonic::include_proto!("state_manager");
 }
 
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Args {
+  #[clap(short, long)]
+  port: u16,
+
+  #[clap(short, long, default_value = "/run/state-manager")]
+  db_path: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let addr = "[::1]:50051".parse()?;
-  let service = GrpcService::new(InMemoryStateManager::default());
+  let args = Args::parse();
+
+  let addr = format!("[::1]:{}", args.port).parse()?;
+  let service = GrpcService::new(PersistentStateManager::<RocksdbStorage>::new(args.db_path));
 
   println!("Listening on {}", addr);
 
